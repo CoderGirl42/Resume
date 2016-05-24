af5a9c5562df4d76ad5ce9586e5715146dc4b240
Marissa du Bois <marissa@galacticsoft.net>
2016-05-24 15:41:41 -0700

    Removed Old Files

--- ArtAndLogic/Alpc1/stdafx.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2014 Art & Logic, Inc. All Rights Reserved.
-// $Id$
-
-// stdafx.cpp : source file that includes just the standard includes
-// Alpc1.pch will be the pre-compiled header
-// stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
